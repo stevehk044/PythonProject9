9b7fcf0c08c697a867bc691b6ff8d72e4cd0856a
stevehk044 <stevehk044@gmail.com>
2024-11-24 17:25:13 -0500

    Add files via upload

---
+++ Assignment-9.py
@@ -0,0 +1,26 @@
+# Prompt the user to enter a single char
+
+char_input = input("Enter a single character: ")
+
+# Check if the input is exactly 1 character and if it is a letter
+
+if len(char_input) == 1 and char_input.isalpha():
+    char = char_input.lower()
+
+    # Check if the char is a vowel
+    if char in "aeiou":
+        print("The character is a vowel")
+
+    else:
+        print("The character is a consonant")
+else:
+    if len(char_input) != 1:
+        print("Error: Please enter only one character")
+    else:
+        print("Error: The input is not a letter")
+
+
+
+
+
+
